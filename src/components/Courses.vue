8a9ffabdd3b7906a372f6703d91cdeff3be4b428
RaulEK <kattai.raul@gmail.com>
2019-11-05 21:09:45 +0200

    task 2

---
+++ src/components/Courses.vue
@@ -0,0 +1,59 @@
+<template>
+    <div id="courses-container" class="tab active">
+        <h1 class="title">Courses</h1>
+        <table id="courses">
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>Course Title</th>
+                <th>Semester</th>
+                <th>Grade</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>1</td>
+                <td>Agile software development</td>
+                <td>1</td>
+                <td>82</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>System modeling</td>
+                <td>1</td>
+                <td>85</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>Object-oriented programming</td>
+                <td>2</td>
+                <td>99</td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>Estonian language Level A2</td>
+                <td>2</td>
+                <td>65</td>
+            </tr>
+            </tbody>
+        </table>
+        <button id="add-course-button" class="blue-button">+</button>
+        <span id="add-course">
+                                <input class="input" type="text" placeholder="Course title" id="title">
+                                <input class="input" type="number" min="1" max="8" placeholder="Semester" id="semester">
+                                <input class="input" type="number" min="0" max="100" placeholder="Grade" id="grade">
+                                <button class="green-button" id="save-course">Save</button>
+                                <button class="grey-button" id="cancel-course">Cancel</button>
+                            </span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Courses"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Profile.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="profile-container" class="tab active">
+        <div id="profile">
+            <div class="avatar">
+                <img src="../assets/me.png" id="picture" alt="My picture">
+            </div>
+            <div class="info">
+                <ul>
+                    <li id="name">John Doe</li>
+                    <li id="birthdate">11/10/1990</li>
+                    <li id="faculty">Software Engineering</li>
+                </ul>
+            </div>
+            <div id="gpa">
+                <strong>2.75</strong>
+            </div>
+            <div class="clear-fix"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Profile"
+    }
+</script>
+
+<style scoped>
+
+</style>
